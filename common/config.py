017ab62b201d68f81fdbaa841d7d42f50093ae8c
kittanh <ngtranganh22@gmail.com>
2023-04-06 15:17:11 +0200

    cassandra

--- common/config.py
+++ common/config.py
@@ -5,7 +5,7 @@ ingestPort = 10001
 kafkaHost = "localhost:9092"
 ingestTopic = "article-ingest"
 
-cassandra_keyspace = "article"
+cassandra_keyspace = "project"
 cassandra_table_article = cassandra_keyspace + ".article"
 cassandra_table_feed = cassandra_keyspace + ".feed"
 cassandra_table_user = cassandra_keyspace + ".user"

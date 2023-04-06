1fd18e11928c9244606d52c70d8bf1ec309ddcb7
kittanh <ngtranganh22@gmail.com>
2023-04-06 17:37:37 +0200

    scraping modifie

--- scraping.py
+++ scraping.py
@@ -1,24 +1,20 @@
 import feedparser
-
-
-def scraping_feed(urls):
-    for url in urls:
-        feed = feedparser.parse(url)
-        articles =[]
+import requests
+import json
+def scraping_feed(url):
     
-        for entry in feed.entries:
-            article = {
-                "title": entry.title,
-                "link": entry.link,
-                "pubDate": entry.published,
-                "description": entry.summary,
-                "guid": entry.id
-             }
-            articles.append(article)
-    return articles
-
-
-
-
-
-       
+    feed = feedparser.parse(url)
+    articles =[]
+    
+    for entry in feed.entries:
+        article = {
+            "title": entry.title,
+            "link": entry.link,
+            "pubDate": entry.published,
+            "description": entry.summary,
+            "guid": entry.id
+        }
+        articles.append(article)
+    url = 'http://localhost:8888/articles'
+    headers = {'Content-Type': 'scraping_flux_RSS'}
+    return requests.post(url, data=json.dumps(articles), headers=headers)

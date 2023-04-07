ac5317c646fbe3758073ce26e26179ded7694fa1
emyregna <118170240+emyregna@users.noreply.github.com>
2023-04-07 12:45:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,113 +1,51 @@
-#Dependencies
-from flask import Flask,render_template,session,redirect,url_for, request, jsonify,send_from_directory
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList
-from wtforms.validators import DataRequired
+import json
+from http import HTTPStatus
 
 from cassandra.cluster import Cluster
-from common.article import Article
+from flask import Flask, Response
+from flask_classful import FlaskView, route
+
+from common import config
 from common.repository import ArticleRepository
-from ingest.producer import send_to_producer
 
-# App
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "secret key"
 
-#Create a Form Class
-class NameForm(FlaskForm):
-    user_id = StringField("Put your id", validators=[DataRequired()])
-    submit = SubmitField("Login")
-
-class ArticleForm(FlaskForm):
-
+class ApiView(FlaskView):
     repository: ArticleRepository = None
 
-    link = StringField("Put your link", validators=[DataRequired()])
-    submit = SubmitField("Save")
-    
-    def save(self):
-        # link = Article(**self.link) #la classe Article dans common article.py
-        # self.__class__.repository.saveArticles(self, link)
-        send_to_producer(self.link)
+    # @route("/stocks/_count", methods=['GET'])
+    # def count_stocks(self):
+    #     result = len(self.__class__.repository.find_all())
 
-class Article_IdForm(FlaskForm):
-    article_id = StringField("Article_id you want to read", validators=[DataRequired()])
-    submit = SubmitField("Search")
+    #     return Response(json.dumps({'count': result}), mimetype="application/json")
 
+    #récupérer les détails d'un article
+    @route("/articles/<article_id>", methods=['GET'])
+    def article(self, article_id: str):
+        result = self.__class__.repository.findOneArticle(article_id)
 
-#Routers
+        if result is None:
+            return Response(json.dumps({'article_id': article_id, 'error': "not found"}), mimetype="application/json",
+                            status=HTTPStatus.NOT_FOUND)
+        else:
+            return Response(json.dumps(result.toDict()), mimetype="application/json")
 
-#Home
-@app.route("/",methods=["GET"])
+    #last 10 article summaries
+    @route("/articles/articles?user_id=<user_id>", methods=['GET'])
+    def last_10_articles_summaries(self, user_id: str):
+        results = self.__class__.repository.findLast10ArticleSummaries(user_id)
 
-def home():
-    return render_template("home.html")
-
-#login
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    name = None
-    form = NameForm()
-    #Validate Form
-    if  request.method == 'POST':
-        name = form.user_id.data
-        form.user_id.data = ''
-        session['name'] = name
-        print(f'Setting session name to {name}')
-        return redirect(url_for('user'))
-    print(f'Invalid form data: {form.errors}')
-    return render_template("login.html",
-                           form=form)
-
-#user page / save articles
-@app.route("/articles", methods=['GET', 'POST'])
-def user():
-    form = ArticleForm()
-    name = session.get('name')
-    print(f'Got session name: {name}')
-    if not name:
-        return redirect(url_for('login'))
-    if request.method == "POST":
-        form.save()
-        form.link.data = ''
-    return render_template('user.html', user_id=name, form=form)
-
-#Show one article given its id
-@app.route("/articles/test", methods=['GET', 'POST'])
-def one_article():
-    form = Article_IdForm()
-    bdd = Cluster()
-    session = bdd.connect()
-    rep = ArticleRepository(session)
-    article=None
-    if request.method=="POST":
-        id = form.article_id
-        article = rep.findOneArticle(id) #Faudrait que la fonction retourne un "article erreur" si l'id n'existe pas 
-    return render_template("FormOneArticle.html", form=form)
-
-#user's 10 last articles
-@app.route("/articles/userArticles")
-def ten_articles():
-    ID = session.get("name")
-    bdd = Cluster()
-    session = bdd.connect()
-    rep = ArticleRepository(session)
-    articles = rep.findLast10ArticleSummaries(ID)
-    return render_template("TenArticles.html", articles=articles)
-
-#Create custom error pages
-
-#Invalid URL
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-#Internal Server Error
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template("500.html"), 500
+        return Response(json.dumps([s.toDict() for s in results]), mimetype="application/json")
 
 
-#Run
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == '__main__':
+    port = config.apiPort
+
+    cluster = Cluster()
+    connection = cluster.connect()
+    try:
+        app = Flask(__name__)
+        ApiView.repository = ArticleRepository(connection)
+        ApiView.register(app, route_base="/api")
+        app.run("0.0.0.0", port)
+    finally:
+        cluster.shutdown()

10a702f4adb4d03a7e9a90ad8c9ab7505f2dbf1a
emyregna <118170240+emyregna@users.noreply.github.com>
2023-04-07 12:53:28 +0200

    Add files via upload

---
+++ init_service.py
@@ -0,0 +1,18 @@
+from cassandra.cluster import Cluster
+
+if __name__ == '__main__':
+    cassandra = Cluster()
+    connection = cassandra.connect()
+    try:
+        connection.execute("CREATE KEYSPACE IF NOT EXISTS project  WITH replication = {'class':'SimpleStrategy', 'replication_factor':1}")
+        connection.execute("CREATE TABLE IF NOT EXISTS project.feed(feed_id text PRIMARY KEY)")
+        connection.execute("CREATE TABLE IF NOT EXISTS project.user_feed(user_id text, feed_id text, PRIMARY KEY(user_id, feed_id)));")
+            
+        connection.execute("CREATE TABLE IF NOT EXISTS project.user(user_id text PRIMARY KEY)")
+        connection.execute("CREATE TABLE IF NOT EXISTS project.article(article_id text, feed_id text, title text, pubDate date, description text, link text, PRIMARY KEY ((feed_id), article_id))")
+
+        connection.execute("")
+
+
+    finally:
+        connection.shutdown()

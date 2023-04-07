fe4c7b2e98d1d0e69d1dd9d3e338e5c679f1064a
emyregna <118170240+emyregna@users.noreply.github.com>
2023-04-07 12:52:13 +0200

    Add files via upload

---
+++ producer.py
@@ -0,0 +1,61 @@
+import json 
+import random
+import string
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+from datetime import datetime
+import feedparser
+
+import json
+
+
+def scraping_feed(url):
+    # for url in urls:
+    feed = feedparser.parse(url)
+    rss_id =  ''.join(random.choice(string.ascii_letters) for i in range(32))
+    articles =[]
+
+    for entry in feed.entries:
+        article = {
+            "feed_id": rss_id ,
+            "title": entry.title,
+            "link": entry.link,
+            "pubDate": datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z').strftime('%Y-%m-%d'),
+            "description": entry.summary,
+            "article_id": entry.id
+        }
+        articles.append(article)
+    return articles
+
+
+# On crée le Kafka Producer
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+)
+
+def send_to_producer(url, name):
+    articles = scraping_feed(url)
+
+    #on envoie chaque article dans le producer kafka pour les stocker
+    for article in articles:
+        article['user_id'] = name
+        try:
+            producer.send('flux_rss', key = article['article_id'].encode(), value=json.dumps(article).encode())
+        except KafkaError as e:
+            print(f"Failed to send message to Kafka: {e}")
+        producer.flush()
+
+# if __name__ == '__main__':
+
+    # url = "https://www.cert.ssi.gouv.fr/alerte/feed/"
+    # #On appelle la fonction qui permet de scrapper les feeds 
+    # articles = scraping_feed(url)
+
+
+    # #on envoie chaque article dans le producer kafka pour les stocker
+    # for article in articles:
+    #     try:
+    #         producer.send('flux_rss', key = article['article_id'].encode(), value=json.dumps(article).encode())
+    #     except KafkaError as e:
+    #         print(f"Failed to send message to Kafka: {e}")
+    #     producer.flush()

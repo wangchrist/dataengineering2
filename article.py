09cbe5717cd64d7a5e451d845169eff9e6779076
emyregna <118170240+emyregna@users.noreply.github.com>
2023-04-07 12:48:47 +0200

    Add files via upload

---
+++ article.py
@@ -0,0 +1,52 @@
+import json
+from datetime import datetime
+
+class Article(object):
+    feed_id: str
+    article_id: str
+    title: str
+    pubDate: datetime
+    description: str
+    link: str
+    
+
+    def __init__(self, feed_id: str, article_id: str, title: str, pubDate: datetime, description: str, link: str):
+        self.feed_id = feed_id
+        self.article_id = article_id
+        self.title = title
+        self.pubDate = pubDate
+        self.description = description
+        self.link = link
+
+    def __str__(self):
+        return "%s(%s,%s)" % (self.__class__.__name__, self.id, self.quantity)
+
+    def toDict(self):
+        return {'feed_id': self.feed_id, 'article_id': self.article_id, 'title': self.title, 'pubDate': self.pubDate, 'description': self.description, 'link': self.link}
+
+    def toJson(self):
+        return json.dumps(self.toDict())
+
+
+
+class ArticleSummary(object):
+    feed_id: str
+    article_id: str
+    title: str
+    pubDate: datetime
+    
+
+    def __init__(self, feed_id: str, article_id: str, title: str, pubDate: datetime):
+        self.feed_id = feed_id
+        self.article_id = article_id
+        self.title = title
+        self.pubDate = pubDate
+
+    def __str__(self):
+        return "%s(%s,%s)" % (self.__class__.__name__, self.id, self.quantity)
+
+    def toDict(self):
+        return {'feed_id': self.feed_id, 'article_id': self.article_id, 'title': self.title, 'pubDate': self.pubDate}
+
+    def toJson(self):
+        return json.dumps(self.toDict())
---
+++ repository.py
@@ -0,0 +1,56 @@
+from typing import *
+
+from cassandra.cluster import Session
+
+from common import config
+from common.article import Article, ArticleSummary
+
+
+class ArticleRepository(object):
+    def __init__(self, connection: Session):
+        self.__connection = connection
+
+
+    def findOneArticle(self, article_id: str) -> Optional[Article]:
+        row = self.__connection.execute(
+            "SELECT feed_id, title, pubDate, description, link FROM project.article WHERE article_id = %s",
+            (article_id)
+        ).one()
+
+        if row is None:
+            return None
+        else:
+            (feed_id, article_id, title, pubDate, description, link) = row
+            return Article(feed_id=feed_id, article_id=article_id, title=title, pubDate=pubDate, description=description, link=link)
+
+
+    def findLast10ArticleSummaries(self, user_id: str) -> List[ArticleSummary]:
+        rows = self.__connection.execute(
+            # "SELECT feed_id, article_id, title, pubDate FROM " + config.cassandra_table + " ORDER BY pubDate DESC LIMIT 10"
+            # "SELECT article.feed_id, article.article_id, article.title, article.pubDate" +
+            #     "FROM project.article" +
+            #     "JOIN project.feed ON feed.feed_id = article.feed_id" +
+            #     "JOIN project.user ON user.user_id = feed.user_id" +
+            #     "WHERE user.user_id = " + user_id +
+            #     " ORDER BY article.pubDate DESC LIMIT 10;"
+
+                "SELECT article_id, pubDate, title, description, link FROM project.article" +
+                "WHERE feed_id IN (SELECT feed_in FROM project.user_feed WHERE user_id =" + user_id +
+                ")ORDER BY pubDate DESC LIMIT 10;"
+                
+                # pas bon refaire avec la nouvelle bdd
+        ).all()
+
+        result = []
+        for (feed_id, article_id, title, pubDate) in rows:
+            result.append(ArticleSummary(feed_id=feed_id, article_id=article_id, title=title, pubDate=pubDate))
+
+        return result
+
+
+    def saveArticles(self, articles: List[Article]):
+        for article in articles:
+            self.__connection.execute(
+                "INSERT INTO project.table (feed_id, article_id, title, pubDate, description, link) VALUES (%s, %s, %s, %s, %s, %s);",
+                (article.feed_id, article.article_id, article.title, article.pubDate, article.description, article.link)
+            )
